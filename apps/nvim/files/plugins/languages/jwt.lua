87f5083a1a32996a74bc8958df65634bbb4a37f6
Benjamin Stürmer <benjamin@stuermer.pro>
2024-10-26 21:19:14 +0200

    remove mersge

--- apps/nvim/files/plugins/languages/jwt.lua
+++ apps/nvim/files/plugins/languages/jwt.lua
@@ -4,14 +4,6 @@ return {
         version = "*", 
         lazy = false,
         config = function()
-            function _mersge_toggle()
-                local term = require("toggleterm.terminal").Terminal
-                local mersge = term:new({ direction = "float", cmd = "mersge .", hidden = true })
-
-                mersge:toggle()
-            end
-            vim.api.nvim_set_keymap("n", "<leader>gm", "<cmd>lua _mersge_toggle()<CR>", {noremap = true, silent = true})
-
             function _jwtui_toggle()
                 -- all lines in the current buffer
                 local lines = table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, false), '')
