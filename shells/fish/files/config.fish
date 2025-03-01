ddddaf08f0a393c856fdbc00cd3e42c68433ba3e
Benjamin Stürmer <benjamin@stuermer.pro>
2025-03-01 22:15:49 +0100

    add fish

--- shells/fish/files/config.fish
+++ shells/fish/files/config.fish
@@ -14,16 +14,9 @@ fzf --fish | source
 # https://github.com/cantino/mcfly
 mcfly init fish | source
 
-# notifications for long running tasks
-# https://github.com/franciscolourenco/done
-# fisher install franciscolourenco/done
-
-# Speed up git flow
-# https://github.com/joseluisq/gitnow
-# fisher install joseluisq/gitnow@2.12.0
-
 abbr --add get_idf '. $HOME/esp/esp-idf/export.sh'
 abbr --add update-submodules 'git submodule sync; git submodule update --init'
 abbr --add mp3 "yt-dlp -i --extract-audio --audio-format mp3 $1"
 abbr --add scpresume "rsync --partial --progress "
 abbr --add reload exec fish
+
